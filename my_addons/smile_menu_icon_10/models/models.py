a5b47d578d6ff80f15ffadd5d4afb45449806d06
Youssef ECHCHERIFELKETTANI <youssef.echcherifelkettani@smile-maroc.com>
2018-07-10 16:57:54 +0100

    [UPD]

---
+++ my_addons/smile_menu_icon_10/models/models.py
@@ -0,0 +1,94 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2010 Smile (<http://www.smile.fr>). All Rights Reserved
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+import operator
+
+from odoo import api, fields, models, tools, _
+
+
+class IrUiMenu(models.Model):
+    _inherit = 'ir.ui.menu'
+
+    fa_icon = fields.Char('Font Awesome Icon', help='Given icon must appear on the left of menu label.')
+
+
+    # DIRTYYYY ! TO IMPROVE !! It's just a copy-paste of the natives functions. (with some modifications)
+
+    @api.model
+    @tools.ormcache_context('self._uid', keys=('lang',))
+    def load_menus_root(self):
+        fields = ['name', 'sequence', 'parent_id', 'action', 'web_icon_data', 'fa_icon']
+        menu_roots = self.get_user_roots()
+        menu_roots_data = menu_roots.read(fields) if menu_roots else []
+        return {
+            'id': False,
+            'name': 'root',
+            'parent_id': [-1, ''],
+            'children': menu_roots_data,
+            'all_menu_ids': menu_roots.ids,
+        }
+
+    @api.model
+    @tools.ormcache_context('self._uid', 'debug', keys=('lang',))
+    def load_menus(self, debug):
+        """ Loads all menu items (all applications and their sub-menus).
+
+        :return: the menu root
+        :rtype: dict('children': menu_nodes)
+        """
+        fields = ['name', 'sequence', 'parent_id', 'action', 'web_icon', 'web_icon_data', 'fa_icon']
+        menu_roots = self.get_user_roots()
+        menu_roots_data = menu_roots.read(fields) if menu_roots else []
+        menu_root = {
+            'id': False,
+            'name': 'root',
+            'parent_id': [-1, ''],
+            'children': menu_roots_data,
+            'all_menu_ids': menu_roots.ids,
+        }
+        if not menu_roots_data:
+            return menu_root
+
+        # menus are loaded fully unlike a regular tree view, cause there are a
+        # limited number of items (752 when all 6.1 addons are installed)
+        menus = self.search([('id', 'child_of', menu_roots.ids)])
+        menu_items = menus.read(fields)
+
+        # add roots at the end of the sequence, so that they will overwrite
+        # equivalent menu items from full menu read when put into id:item
+        # mapping, resulting in children being correctly set on the roots.
+        menu_items.extend(menu_roots_data)
+        menu_root['all_menu_ids'] = menus.ids  # includes menu_roots!
+
+        # make a tree using parent_id
+        menu_items_map = {menu_item["id"]: menu_item for menu_item in menu_items}
+        for menu_item in menu_items:
+            parent = menu_item['parent_id'] and menu_item['parent_id'][0]
+            if parent in menu_items_map:
+                menu_items_map[parent].setdefault(
+                    'children', []).append(menu_item)
+
+        # sort by sequence a tree using parent_id
+        for menu_item in menu_items:
+            menu_item.setdefault('children', []).sort(key=operator.itemgetter('sequence'))
+
+        return menu_root
+
