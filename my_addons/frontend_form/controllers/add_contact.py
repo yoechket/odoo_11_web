3e09479a036c400cf8f9cdb5dc358c321661d82f
Youssef ECHCHERIFELKETTANI <youssef.echcherifelkettani@smile-maroc.com>
2018-07-16 09:42:10 +0100

    [UPD]

--- my_addons/frontend_form/controllers/add_contact.py
+++ my_addons/frontend_form/controllers/add_contact.py
@@ -4,6 +4,7 @@ from odoo.http import request
 
 
 class Contact(http.Controller):
+
     @http.route("/add_contact", type="http", auth="user", website=True)
     def add_contact(self):
         contact_country_ids = request.env['res.country'].sudo().search([])
@@ -61,3 +62,66 @@ class Contact(http.Controller):
             elif not lead_ids:
                 lead_id = request.env['crm.lead'].sudo().create({'partner_id': partner_id.id,
                                                                  'name': lead_name})
+                return request.render(
+                    'frontend_form.existing_contact',
+                    {'partner_id': partner_id,
+                     'lead_id': lead_id}
+                )
+
+    @http.route("/add_contact/create_contact_modal",
+                type="http", auth="user",
+                website=True, method="post")
+    def create_contact_modal(self, contact_name,
+                       contact_street,
+                       contact_city,
+                       contact_country,
+                       contact_phone,
+                       contact_email,
+                       lead_name,
+                       **post):
+        partner_ids = request.env['res.partner'].sudo().search([
+            ('name', '=', contact_name)
+        ])
+        if not partner_ids:
+            partner_id = request.env['res.partner'].sudo().create({'name': contact_name,
+                                                                   'street': contact_street,
+                                                                   'city': contact_city,
+                                                                   'country_id': contact_country,
+                                                                   'phone': contact_phone,
+                                                                   'email': contact_email})
+            lead_ids = request.env['crm.lead'].sudo().search([
+                ('name', '=', lead_name)
+            ])
+            if lead_ids:
+                lead_id = lead_ids[0]
+                lead_id.sudo().write({'partner_id': partner_id.id})
+            elif not lead_ids:
+                lead_id = request.env['crm.lead'].sudo().create({'partner_id': partner_id.id,
+                                                                 'name': lead_name})
+                return request.render(
+                    'frontend_form.contact_created',
+                    {'partner_id': partner_id,
+                     'lead_id': lead_id})
+        elif partner_ids:
+            partner_ids = request.env['res.partner'].sudo().search([
+                ('name', '=', contact_name)
+            ])
+            partner_id = partner_ids[0]
+            lead_ids = request.env['crm.lead'].sudo().search([
+                ('name', '=', lead_name)
+            ])
+            if lead_ids:
+                lead_id = lead_ids[0]
+                return request.render(
+                    'frontend_form.existing_contact',
+                    {'partner_id': partner_id,
+                     'lead_id': lead_id}
+                )
+            elif not lead_ids:
+                lead_id = request.env['crm.lead'].sudo().create({'partner_id': partner_id.id,
+                                                                 'name': lead_name})
+                return request.render(
+                    'frontend_form.existing_contact',
+                    {'partner_id': partner_id,
+                     'lead_id': lead_id}
+                )
--- my_addons/frontend_form/models/contact_group.py
+++ my_addons/frontend_form/models/contact_group.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from odoo import fields, models, api
+from odoo.exceptions import UserError
 import uuid
 
 
@@ -8,6 +9,7 @@ class contactGroup(models.Model):
 
     name = fields.Char(string='Name', required=True)
     partner_ids = fields.One2many('res.partner', 'group_id', string='Contacts')
+    rank = fields.Integer(string='Rank')
 
     def _get_default_access_token(self):
         uid = str(uuid.uuid4())
@@ -23,3 +25,8 @@ class contactGroup(models.Model):
             'target': 'self',
             'url': '/group/%s/%s' % (self.id, self.access_token)
         }
+
+    @api.constrains('rank')
+    def _check_rank(self):
+        if self.rank < 1:
+            raise UserError('Rank needs to be a positive number!')
